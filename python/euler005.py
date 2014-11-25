0f85fff75b346b142e7d5145cecbba8a69e0388c
hails <stuart.vonn@gmail.com>
2014-11-25 12:30:06 -0200

    Update euler005.py

--- python/euler005.py
+++ python/euler005.py
@@ -5,12 +5,10 @@ def is_evendiv(number):
     for num in xrange(1, 21):
         if number%num != 0:
             return False
-
     return True
 
 
 start = 20
-
 while(not is_evendiv(start)):
     start += 20
 
