becb79f559b0f5defd3834cf66ae3917eb45300d
brkk/hails <stuart.vonn@gmail.com>
2014-11-25 12:27:43 -0200

    Problem 5 solution

---
+++ python/euler005.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+
+def is_evendiv(number):
+    for num in xrange(1, 21):
+        if number%num != 0:
+            return False
+
+    return True
+
+
+start = 20
+
+while(not is_evendiv(start)):
+    start += 20
+
+print start
